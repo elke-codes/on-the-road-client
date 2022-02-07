bf7039ea1f062d3fd9db5aa643e08548823a9f2f
Elke Dick <elke.codes@gmail.com>
2022-02-07 00:42:48 -0800

    https timezonedb

--- src/utils/time/timeAtLocation.js
+++ src/utils/time/timeAtLocation.js
@@ -7,7 +7,7 @@ const API_KEY = process.env.REACT_APP_TIMEZONEDB_API_KEY;
 export const timeAtLocation = async (selectedFriendLat, selectedFriendLng) => {
 	const localTime = await axios
 		.get(
-			`http://api.timezonedb.com/v2.1/get-time-zone?key=${API_KEY}&format=json&by=position&lat=${selectedFriendLat}&lng=${selectedFriendLng}`
+			`https://api.timezonedb.com/v2.1/get-time-zone?key=${API_KEY}&format=json&by=position&lat=${selectedFriendLat}&lng=${selectedFriendLng}`
 		)
 		.then((result) => {
 			const formatted = result.data.formatted;
