47787345d093bc8fa29ebdb16c42c7103e58add0
Elke Dick <elke.codes@gmail.com>
2022-04-13 22:27:24 -0700

    about mobile

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -2,16 +2,23 @@
 import "./AboutPage.scss";
 import React from "react";
 import Footer from "../../components/Footer/Footer";
-import me from "../../assets/images/skye 2015.jpg";
+import me from "../../assets/images/elke.jpg";
+import gitHub from "../../assets/icons/GitHub-Mark-32px.png";
+import linkedIn from "../../assets/icons/linkedin.png";
 
 const AboutPage = () => {
 	return (
 		<>
 			<main className="about">
 				<article className="about__main">
-					<h1 className="about__title">On the road...</h1>
+					<h3 className="about__copy--highlight about__copy--bold">
+						wayward is here for people who move around, connect and
+						love to reconnect, who are interested in the social, and
+						less in the media.
+					</h3>
+					{/* <h1 className="about__title">wayward...</h1> */}
 					<p className="about__copy">
-						... while travelling, or moving around this wonderful,
+						While travelling, or moving around this wonderful,
 						enticing globe for business, inevitable you make some
 						meaningful connections. You want to stay in touch, but
 						how? Email? Am I really going to send this person an
@@ -38,25 +45,20 @@ const AboutPage = () => {
 						cheers, I was hoping some of those travelling souls I
 						met along the way would respond, and we could reconnect.
 						<h3 className="about__copy--highlight">
-							... enter: On the road...
+							... enter: wayward...
 						</h3>
-						With on the road you can stay touch with people you met
-						on the road, chat with them AND see where in the world
-						they are right now! <br />
+						With wayward you can stay touch with people you met on
+						the road, chat with them AND see where in the world they
+						are right now! <br />
 						Have a 48h layover in London? See who's around, reach
 						and have them show you around. Moving to Madrid and
 						wondering if you know someone there? You get the
 						picture.
 					</p>
-					<h3 className="about__copy--highlight about__copy--bold">
-						On the road is here for people who move around, connect
-						and love to reconnect, who are interested in the social,
-						and less in the media.
-					</h3>
 				</article>
 				<aside className="about__me">
 					<div className="about__me-header">
-						<h2 className="about__me-title">About me</h2>
+						<h2 className="about__me-title">About me </h2>
 						<img src={me} alt="" className="about__me-img" />
 					</div>
 					<p>
@@ -67,6 +69,39 @@ const AboutPage = () => {
 						great, useful product.
 					</p>
 				</aside>
+				<article className="about__connect">
+					<div className="about__connect-header">
+						<h2 className="about__connect-title">LET'S CONNECT </h2>
+					</div>
+					<div className="about__connect-body">
+						<a
+							href="https://github.com/elke-codes"
+							target="_blank"
+							title="See my projects on GitHub">
+							<img
+								src={gitHub}
+								alt="github logo"
+								className="about__connect-img"
+							/>
+						</a>
+						<a
+							href="https://www.linkedin.com/in/elke-dick/"
+							target="_blank"
+							title="Get to know me better on LinkedIn">
+							<img
+								src={linkedIn}
+								alt="linkedin logo"
+								className="about__connect-img"
+							/>
+						</a>
+					</div>
+					<a
+						href="mailto:elke.codes@gmail.com"
+						target="_blank"
+						title="Email me!">
+						elke.codes@gmail.com
+					</a>
+				</article>
 			</main>
 			<Footer />
 		</>
